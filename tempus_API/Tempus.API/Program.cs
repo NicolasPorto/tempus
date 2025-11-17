27ab238cbc8a395fca8a9065d8e2961ce4684577
LuisQuintino <luisvq12333@gmail.com>
2025-11-17 10:37:50 -0300

    Alterando dockerfile, gerando fly.toml e botyando swagger no publicado

--- tempus_API/Tempus.API/Program.cs
+++ tempus_API/Tempus.API/Program.cs
@@ -27,12 +27,10 @@ InjectRepositories(builder.Services);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.UseHttpsRedirection();
 
